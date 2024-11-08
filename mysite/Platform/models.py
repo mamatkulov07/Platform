6313e2fadaa57e8e8080650f688a56f66fe4bd92
Usman <usmanmamatkulov4.com>
2024-11-08 15:09:31 +0600

    k

--- mysite/Platform/models.py
+++ mysite/Platform/models.py
@@ -5,20 +5,20 @@ class Home(models.Model):
     description = models.TextField()
     name = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='homeimage/')
+    image = models.ImageField(upload_to='homeimage/', null=True, blank=True)
     title = models.CharField(max_length=100)
-    video_file = models.FileField(upload_to='videos/')
+    video_file = models.FileField(upload_to='videos/', null=True, blank=True)
     exams = models.TextField()
-    examsimage = models.ImageField(upload_to='examsimage/')
+    examsimage = models.ImageField(upload_to='examsimage/', null=True, blank=True)
 
     def __str__(self):
         return self.name
 
 
 class AboutUs(models.Model):
-    image = models.ImageField(upload_to='aboutusimage/')
+    image = models.ImageField(upload_to='aboutusimage/', null=True, blank=True)
     descriptions = models.TextField()
-    personimage = models.ImageField(upload_to='personimage/')
+    personimage = models.ImageField(upload_to='personimage/', null=True, blank=True)
     nameperson = models.CharField(max_length=100)
     profession = models.CharField(max_length=200)
 
@@ -28,7 +28,7 @@ class AboutUs(models.Model):
 
 class StudyAbroad(models.Model):
     description = models.TextField()
-    image = models.ImageField(upload_to='image/')
+    image = models.ImageField(upload_to='image/', null=True, blank=True)
 
     def __str__(self):
         return self.description
@@ -66,7 +66,7 @@ class Country(models.Model):
         ('Canada', 'Canada'),
     )
     countrys = models.CharField(max_length=100, choices=country)
-    image = models.ImageField(upload_to='countryimage/')
+    image = models.ImageField(upload_to='countryimage/', null=True, blank=True)
     studyabroad = models.ForeignKey(StudyAbroad, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -77,7 +77,7 @@ class Contacts(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     phone = models.CharField(max_length=100)
-    email = models.EmailField()
+    email = models.EmailField(max_length=250)
     group = (
         ('Group', 'Group'),
         ('Company', 'Company')
